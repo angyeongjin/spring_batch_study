4a248068dfe2ae0a13b8114786175ea3a8778366
glassesjin <akj0426@gmail.com>
2022-08-17 14:45:36 +0900

    HelloJob 추가

---
+++ src/main/java/com/example/springbatch/config/endpoint/hello/job/HelloJobConfiguration.java
@@ -0,0 +1,40 @@
+package com.example.springbatch.config.endpoint.hello.job;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author An GyeongJin
+ * @date 2022-08-16
+ * @see
+ */
+@Configuration
+@RequiredArgsConstructor
+public class HelloJobConfiguration {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job helloJop() {
+        return jobBuilderFactory.get("helloJob")
+                .start(helloStep())
+                .build();
+    }
+
+    @Bean
+    public Step helloStep() {
+        return stepBuilderFactory.get("helloStep")
+                .tasklet((contribution, chunkContext) -> {
+                    System.out.println("");
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+}
